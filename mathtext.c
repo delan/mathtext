3847e49d7bdd3069ff39bc55021454e9766d8f32
Delan Azabani <delan@azabani.com>
2013-07-21 20:08:37 +0800

    use character tables instead of shifting to solve missing characters

--- mathtext.c
+++ mathtext.c
@@ -1,6 +1,30 @@
+/*
+	Mapped characters:
+	*	U+0021..U+007E (printable ASCII)
+	Would map these characters if I implemented Unicode input:
+	*	U+0131 (LATIN SMALL LETTER DOTLESS I)
+	*	U+0237 (LATIN SMALL LETTER DOTLESS J)
+	*	U+0391..U+03C9 (Greek)
+	*	U+03D1 (GREEK THETA SYMBOL)
+	*	U+03D5 (GREEK PHI SYMBOL)
+	*	U+03D6 (GREEK PI SYMBOL)
+	*	U+03DC (GREEK LETTER DIGAMMA)
+	*	U+03DD (GREEK SMALL LETTER DIGAMMA)
+	*	U+03F0 (GREEK KAPPA SYMBOL)
+	*	U+03F1 (GREEK RHO SYMBOL)
+	*	U+03F5 (GREEK LUNATE EPSILON SYMBOL)
+	*	U+2202 (PARTIAL DIFFERENTIAL)
+	*	U+2207 (NABLA)
+	Treating input as ASCII for now, the table is a mapping from
+	bytes, not Unicode codepoints, to Unicode codepoints.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "tables.h"
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
 void putchar_unicode(long c) {
 	if (c < 0x80) {
 		putchar(c);
@@ -19,116 +43,33 @@ void putchar_unicode(long c) {
 	}
 }
 int help(void) {
+	int i;
 	puts("mathtext: write styled text with Unicode plain text");
-	puts("Usage: mathtext [type]");
-	puts("[type] can be one of:");
-	puts("    serifb      serifi      serifbi     sans");
-	puts("    sansb       sansi       sansbi      mono");
-	puts("    script      scriptb     double      fraktur");
-	puts("    frakturb    fullwidth");
+	printf("Usage: mathtext [type]\n[type] can be one of:");
+	for (i = 0; i < ARRAY_SIZE(table_names); i++) {
+		if (i % 4 == 0)
+			printf("\n\t");
+		printf("%-16s", table_names[i]);
+	}
+	putchar('\n');
 	return 1;
 }
-void transpose_alphanum(long s, long caps, long lower, long num) {
-	if (s >= 'A' && s <= 'Z')
-		putchar_unicode(s + caps);
-	else if (s >= 'a' && s <= 'z')
-		putchar_unicode(s + lower);
-	else if (s >= '0' && s <= '9')
-		putchar_unicode(s + num);
-	else putchar(s);
-}
-void transpose_block(long s, long min, long max, long delta) {
-	if (s >= min && s <= max)
-		putchar_unicode(s + delta);
-	else putchar(s);
-}
-int main(int argc, char** argv) {
-	long transpose_type = 0,
-		caps, lower, num,
-		min, max, delta,
-		c;
-	if (argc < 2)
-		return help();
-	if (!strcmp(argv[1], "serifb")) {
-		transpose_type = 0;
-		caps = 119808 - 'A';
-		lower = 119834 - 'a';
-		num = 120782 - '0';
-	} else if (!strcmp(argv[1], "serifi")) {
-		transpose_type = 0;
-		caps = 119860 - 'A';
-		lower = 119886 - 'a';
-		num = 0;
-	} else if (!strcmp(argv[1], "serifbi")) {
-		transpose_type = 0;
-		caps = 119912 - 'A';
-		lower = 119938 - 'a';
-		num = 0;
-	} else if (!strcmp(argv[1], "sans")) {
-		transpose_type = 0;
-		caps = 120224 - 'A';
-		lower = 120250 - 'a';
-		num = 120802 - '0';
-	} else if (!strcmp(argv[1], "sansb")) {
-		transpose_type = 0;
-		caps = 120276 - 'A';
-		lower = 120302 - 'a';
-		num = 120812 - '0';
-	} else if (!strcmp(argv[1], "sansi")) {
-		transpose_type = 0;
-		caps = 120328 - 'A';
-		lower = 120354 - 'a';
-		num = 0;
-	} else if (!strcmp(argv[1], "sansbi")) {
-		transpose_type = 0;
-		caps = 120380 - 'A';
-		lower = 120406 - 'a';
-		num = 0;
-	} else if (!strcmp(argv[1], "mono")) {
-		transpose_type = 0;
-		caps = 120432 - 'A';
-		lower = 120458 - 'a';
-		num = 120822 - '0';
-	} else if (!strcmp(argv[1], "script")) {
-		transpose_type = 0;
-		caps = 119964 - 'A';
-		lower = 119990 - 'a';
-		num = 0;
-	} else if (!strcmp(argv[1], "scriptb")) {
-		transpose_type = 0;
-		caps = 120016 - 'A';
-		lower = 120042 - 'a';
-		num = 0;
-	} else if (!strcmp(argv[1], "double")) {
-		transpose_type = 0;
-		caps = 120120 - 'A';
-		lower = 120146 - 'a';
-		num = 120792 - '0';
-	} else if (!strcmp(argv[1], "fraktur")) {
-		transpose_type = 0;
-		caps = 120068 - 'A';
-		lower = 120094 - 'a';
-		num = 0;
-	} else if (!strcmp(argv[1], "frakturb")) {
-		transpose_type = 0;
-		caps = 120172 - 'A';
-		lower = 120198 - 'a';
-		num = 0;
-	} else if (!strcmp(argv[1], "fullwidth")) {
-		transpose_type = 1;
-		min = '!';
-		max = '~';
-		delta = 65281 - '!';
-	} else return help();
+int remap(long *table) {
+	int c;
 	while ((c = getchar()) != EOF) {
-		switch (transpose_type) {
-		case 0:
-			transpose_alphanum(c, caps, lower, num);
-			break;
-		case 1:
-			transpose_block(c, min, max, delta);
-			break;
-		}
+		if (table[c] == -1)
+			putchar_unicode(c);
+		else
+			putchar_unicode(table[c]);
 	}
 	return 0;
+}
+int main(int argc, char **argv) {
+	int i;
+	if (argc != 2)
+		return help();
+	for (i = 0; i < ARRAY_SIZE(table_names); i++)
+		if (!strcmp(table_names[i], argv[1]))
+			return remap(tables[i]);
+	return help();
 }
